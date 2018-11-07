58cdc9c66ad3bf18fb733ba5af7d43ea3cf32d94
rudolf palosaari <rudolf.palosaari@gmail.com>
2018-11-07 17:41:14 +0200

    Post Feed. Like and Unlike.

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,7 +1,11 @@
 import axios from "axios";
-import { ADD_POST, GET_POSTS, POST_LOADING, GET_ERRORS } from "./types";
-import setAuthToken from "../utils/setAuthToken";
-import jwt_decode from "jwt-decode";
+import {
+  ADD_POST,
+  GET_POSTS,
+  POST_LOADING,
+  GET_ERRORS,
+  DELETE_POST
+} from "./types";
 
 // Get all posts
 export const addPost = postData => dispatch => {
@@ -36,6 +40,53 @@ export const getPosts = () => dispatch => {
       dispatch({
         type: GET_POSTS,
         payload: {}
+      });
+    });
+};
+
+export const deletePost = id => dispatch => {
+  axios
+    .delete(`/api/posts/${id}`)
+    .then(res => {
+      dispatch({
+        type: DELETE_POST,
+        payload: id
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+// Add like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => {
+      dispatch(getPosts());
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    });
+};
+
+// Remove like
+export const removeLike = id => dispatch => {
+  axios
+    .post(`/api/posts/unlike/${id}`)
+    .then(res => {
+      dispatch(getPosts());
+    })
+    .catch(err => {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       });
     });
 };
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,9 @@
-import { POST_LOADING, ADD_POST } from "../actions/types";
+import {
+  POST_LOADING,
+  ADD_POST,
+  GET_POSTS,
+  DELETE_POST
+} from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -12,6 +17,22 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: [action.payload, ...state.posts]
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        loading: false,
+        posts: action.payload
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
+    case POST_LOADING:
+      return {
+        ...state,
+        loading: true
       };
     default:
       return state;
